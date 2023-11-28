968c2fa463474a8ad05b50655c8f4ce51bd14317
SNJY <112568579+Ericsnjy@users.noreply.github.com>
2023-11-29 00:36:05 +0700

    Main.java

--- Main.java
+++ Main.java
@@ -4,11 +4,14 @@ package tugaspbo_kel6;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+
 public class Main {
     public static void main(String[] args) {
-       
+        
         Buku buku1 = new Buku("Hujan", "Tere Liye", "9786020324784");
         Buku buku2 = new Buku("Pulang", "Tere Liye", "9786020822129");
+        
+        System.out.println("\n==================INFORMASI BUKU==================\n");
 
         buku1.informasiBuku();
         buku1.cekKetersediaan();
@@ -16,30 +19,25 @@ public class Main {
         buku2.informasiBuku();
         buku2.cekKetersediaan();
 
-        System.out.println("\n====================================\n");
+        System.out.println("\n===============CATATAN PEMINJAMAN BUKU===================\n");
 
-        
         AnggotaPerpustakaan anggota1 = new AnggotaPerpustakaan("Safira Aulia", 123456, "Jl. Mataram No.8");
-
-
         String tanggal = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
-
 
         TransaksiPeminjaman transaksi1 = new TransaksiPeminjaman("Safira Aulia", "28-11-2023",  3, anggota1, buku1);
 
         transaksi1.catatPeminjaman();
         
-        System.out.println("\n====================================\n");
+        System.out.println("\n=============CATATAN PENGEMBALIAN BUKU================\n");
 
-        TransaksiPengembalian pengembalian1 = new TransaksiPengembalian("1-12-2023", anggota1, buku1);        
+        TransaksiPengembalian pengembalian1 = new TransaksiPengembalian("1-12-2023", anggota1, buku1);
         pengembalian1.catatPengembalian();
 
-        System.out.println("\n====================================\n");
+        System.out.println("\n================RIWAYAT PEMINJAMAN===================\n");
 
-        
         anggota1.lihatRiwayatPeminjaman();
 
-        System.out.println("\n====================================\n");
+        System.out.println("\n================NOTIFIKASI====================\n");
         
         Notifikasi notifikasi = new Notifikasi() {
         };
@@ -48,10 +46,10 @@ public class Main {
         notifikasi.setInfo_terbaru("Peminjaman buku Anda segera berakhir.");
 
         notifikasi.kirimNotifikasi();
-        System.out.println("\n====================================\n");
+        System.out.println("\n==================LAPORAN==================\n");
 
         Admin admin = new Admin();
-        
+
         admin.kelolaAnggota();
 
         admin.kelolaBuku();
